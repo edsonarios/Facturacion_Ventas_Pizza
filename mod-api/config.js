4773588d6381c9aaeccb548f18bdb9f1180f8b51
edsonarios <edsonrios9@gmail.com>
2018-12-12 20:46:58 -0400

    arreglado config db
    arreglado config db

--- mod-api/config.js
+++ mod-api/config.js
@@ -4,9 +4,9 @@ const debug = require('debug')('mod:api:db')
 
 module.exports = {
   db: {
-    database: process.env.DB_NAME || 'patelecom',
-    username: process.env.DB_USER || 'patelecom',
-    password: process.env.DB_PASS || 'patelecom',
+    database: process.env.DB_NAME || 'pizza',
+    username: process.env.DB_USER || 'pizza',
+    password: process.env.DB_PASS || 'pizza',
     host: process.env.DB_HOST || 'localhost',
     dialect: 'postgres',
     logging: s => debug(s)
